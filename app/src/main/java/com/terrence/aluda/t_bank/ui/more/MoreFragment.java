30cc8ba7dd79e47b0e9bd67b1ce8135f0157f2e0
Agusioma <terryterrence200@gmail.com>
2021-09-25 23:22:20 +0300

    major UI changes

--- app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
@@ -10,22 +10,38 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.terrence.aluda.t_bank.R;
+import com.terrence.aluda.t_bank.adapters.HomeAdapter;
+import com.terrence.aluda.t_bank.adapters.MoreAdapter;
+import com.terrence.aluda.t_bank.models.home.HomeModel;
+import com.terrence.aluda.t_bank.models.more.MoreModel;
+
+import java.util.ArrayList;
 
 public class MoreFragment extends Fragment {
-
-    private MoreViewModel moreViewModel;
-
+    private RecyclerView moreRV;
+    private ArrayList<MoreModel> moreModelArrayList;
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        moreViewModel = new ViewModelProvider(this).get(MoreViewModel.class);
         View root = inflater.inflate(R.layout.fragment_more, container, false);
-        final TextView textView = root.findViewById(R.id.text_more);
-        moreViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        moreRV = root.findViewById(R.id.moreRV);
+
+        // here we have created new array list and added data to it.
+        moreModelArrayList = new ArrayList<>();
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_call_24));
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_security_24));
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_logout_24));
+
+        // we are initializing our adapter class and passing our arraylist to it.
+       MoreAdapter courseAdapter = new MoreAdapter(this, moreModelArrayList);
+
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        moreRV.setLayoutManager(linearLayoutManager);
+        moreRV.setAdapter(courseAdapter);
         return root;
     }
 }
