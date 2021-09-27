94f795f247331a28c402a8c1ed42d60d8209dcf7
Agusioma <terryterrence200@gmail.com>
2021-09-27 23:42:36 +0300

    major UI changes

--- app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
@@ -27,9 +27,9 @@ public class MoreFragment extends Fragment {
 
         // here we have created new array list and added data to it.
         moreModelArrayList = new ArrayList<>();
-        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_call_24));
-        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_security_24));
-        moreModelArrayList.add(new MoreModel(R.drawable.ic_baseline_logout_24));
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_outline_call_24));
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_outline_security_24));
+        moreModelArrayList.add(new MoreModel(R.drawable.ic_outline_logout_24));
 
         // we are initializing our adapter class and passing our arraylist to it.
        MoreAdapter moreeAdapter = new MoreAdapter(this, moreModelArrayList);
