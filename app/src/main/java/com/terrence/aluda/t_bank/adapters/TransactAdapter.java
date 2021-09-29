17562024e433399c1907fcc1f8968701ed830efb
Agusioma <terryterrence200@gmail.com>
2021-09-29 17:29:35 +0300

    major UI changes

--- app/src/main/java/com/terrence/aluda/t_bank/adapters/TransactAdapter.java
+++ app/src/main/java/com/terrence/aluda/t_bank/adapters/TransactAdapter.java
@@ -1,4 +1,52 @@
 package com.terrence.aluda.t_bank.adapters;
 
-public class TransactAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.terrence.aluda.t_bank.R;
+import com.terrence.aluda.t_bank.models.transact.TransactModel;
+import com.terrence.aluda.t_bank.ui.transaction.TransactionFragment;
+import org.jetbrains.annotations.NotNull;;
+
+import java.util.ArrayList;
+
+public class TransactAdapter extends RecyclerView.Adapter<TransactAdapter.ViewHolder> {
+    private TransactionFragment context;
+    private ArrayList<TransactModel> transModelArrayList;
+
+    public TransactAdapter(TransactionFragment context, ArrayList<TransactModel> transactModelArrayList) {
+        this.context = context;
+        this.transModelArrayList = transactModelArrayList;
+    }
+    @NonNull
+    @NotNull
+    @Override
+    public TransactAdapter.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.transact_list, parent, false);
+        return new TransactAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull TransactAdapter.ViewHolder holder, int position) {
+        TransactModel model = transModelArrayList.get(position);
+        holder.our_image.setImageResource(model.getImage_id());
+        holder.our_label.setText(model.getFirst_label());
+    }
+    public int getItemCount() {
+        return transModelArrayList.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView our_image;
+        private TextView our_label;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            our_image = itemView.findViewById(R.id.imageViewTrans);
+            our_label = itemView.findViewById(R.id.text_trans_list1);
+        }
+    }
 }
--- app/src/main/java/com/terrence/aluda/t_bank/models/transact/TransactModel.java
+++ app/src/main/java/com/terrence/aluda/t_bank/models/transact/TransactModel.java
@@ -1,4 +1,30 @@
 package com.terrence.aluda.t_bank.models.transact;
 
 public class TransactModel {
+    private int image_id;
+    private String first_label;
+
+    // Constructor
+    public TransactModel(String first_label, int image_id) {
+        this.first_label = first_label;
+        this.image_id = image_id;
+    }
+
+    // Getter and Setter
+    public int getImage_id() {
+        return image_id;
+    }
+
+    public void setImage_id(int image_id) {
+        this.image_id = image_id;
+    }
+
+    public String getFirst_label() {
+        return first_label;
+    }
+
+    public void setFirst_label(String first_label) {
+        this.first_label = first_label;
+    }
+
 }
