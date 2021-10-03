c7214a0235a43b7f8a4e076e3e1a07b8d0f48bcc
Agusioma <terryterrence200@gmail.com>
2021-10-03 14:32:38 +0300

    major UI changes

---
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/transaction/WithdrawActivity.java
@@ -0,0 +1,13 @@
+package com.terrence.aluda.t_bank.ui.transaction;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WithdrawActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_withdraw);
+    }
+}
