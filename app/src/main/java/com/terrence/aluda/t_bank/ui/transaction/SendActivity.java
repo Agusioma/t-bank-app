749808f015dd4abc94edd5eb95881e6033e4e034
Agusioma <terryterrence200@gmail.com>
2021-10-05 23:49:06 +0300

    major UI changes

--- app/src/main/java/com/terrence/aluda/t_bank/ui/transaction/SendActivity.java
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/transaction/SendActivity.java
@@ -1,15 +1,18 @@
 package com.terrence.aluda.t_bank.ui.transaction;
 
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.terrence.aluda.t_bank.R;
 
 public class SendActivity extends AppCompatActivity {
-
+    private EditText editDefault;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+    editDefault = findViewById(R.id.edit_sendd);
+    editDefault.setText("1520177747921\nAvailable Balance: 415,757,841.78 KES");
     }
 }
