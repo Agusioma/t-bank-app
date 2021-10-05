1da0279419ecfac1fc3ba1221f6ae995bf0594a7
Agusioma <terryterrence200@gmail.com>
2021-10-05 17:49:57 +0300

    major UI changes

---
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/SendActivity.java
@@ -0,0 +1,13 @@
+package com.terrence.aluda.t_bank.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
