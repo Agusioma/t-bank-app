ab2bdf524192db9c15270eae0d489766321a4b22
munubi254 <munubi254@gmail.com>
2021-10-27 00:05:53 +0300

    retrofit test

---
+++ app/src/main/java/com/terrence/aluda/t_bank/retrofit/APIInterface.java
@@ -0,0 +1,23 @@
+package com.terrence.aluda.t_bank.retrofit;
+
+import com.terrence.aluda.t_bank.login.LoginTest;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+
+    @GET("/api/unknown")
+    Call<LoginTest> doGetListResources();
+
+   /* @POST("/api/users")
+    Call<User> createUser(@Body User user);
+
+    @FormUrlEncoded
+    @POST("/api/users?")
+    Call<UserList> doCreateUserWithField(@Field("name") String name, @Field("job") String job);*/
+}
