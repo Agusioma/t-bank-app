bdb6fd9e7b43c6a4ba9f0592eff1c70002fc59ec
munubi254 <munubi254@gmail.com>
2021-10-29 13:45:03 +0300

    retrofit test

--- app/src/main/java/com/terrence/aluda/t_bank/MainActivity.java
+++ app/src/main/java/com/terrence/aluda/t_bank/MainActivity.java
@@ -1,6 +1,7 @@
 package com.terrence.aluda.t_bank;
 
 import android.os.Bundle;
+import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -9,7 +10,7 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String firstname, lastname, natID, userPassword, regDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,12 +18,27 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_transaction, R.id.navigation_borrow, R.id.navigation_more)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+
+            firstname = extras.getString("firstname");
+            lastname = extras.getString("lastname");
+            natID = extras.getString("natID");
+            userPassword = extras.getString("userPassword");
+            regDate= extras.getString("regDate");
+
+        }
+
+        Toast.makeText(getApplicationContext(), firstname+"\n"+lastname+"\n"+natID+"\n"+regDate, Toast.LENGTH_SHORT).show();
+
     }
 
 }
--- app/src/main/java/com/terrence/aluda/t_bank/ui/login/LoginActivity.java
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.terrence.aluda.t_bank.ui.login;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.terrence.aluda.t_bank.MainActivity;
 import com.terrence.aluda.t_bank.login.LoginTest;
 import com.terrence.aluda.t_bank.retrofit.APIClient;
 import com.terrence.aluda.t_bank.retrofit.APIInterface;
@@ -27,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     private String loggedInCheck, firstname, lastname, natID, userPassword, regDate, phoneParam, passwordParam;
     private EditText editNum, editPassword;
     private Button btnAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +41,6 @@ public class LoginActivity extends AppCompatActivity {
 
         btnAuth.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Perform action on click
-                //Intent activityChangeIntent = new Intent(PresentActivity.this, NextActivity.class);
-                // currentContext.startActivity(activityChangeIntent);
-                //PresentActivity.this.startActivity(activityChangeIntent);
                 sendAuthToken();
             }
         });
@@ -49,34 +48,50 @@ public class LoginActivity extends AppCompatActivity {
 
     private void sendAuthToken() {
         APIInterface apiInterface;
-        Gson gson = new Gson();
         final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
 
         phoneParam = editNum.getText().toString();
         passwordParam = editPassword.getText().toString();
 
-        phoneParam = "254"+phoneParam.substring(phoneParam.length() - 9);
+        phoneParam = "254" + phoneParam.substring(phoneParam.length() - 9);
 
         progressDialog.setMessage("Testing");
         progressDialog.show();
+
         responseArray = new ArrayList<>();
+
         apiInterface = APIClient.getClient().create(APIInterface.class);
-        Toast.makeText(getApplicationContext(), "skrr", Toast.LENGTH_SHORT).show();
         Call<List<LoginTest>> call = apiInterface.doAuthenticate(phoneParam, passwordParam);
+
         call.enqueue(new Callback<List<LoginTest>>() {
             @Override
             public void onResponse(Call<List<LoginTest>> call, Response<List<LoginTest>> response) {
+
                 progressDialog.dismiss();
                 responseArray = response.body();
-                //String testdisplayToken = responseArray.get(0).getFirstname();
-                //Toast.makeText(getApplicationContext(), testdisplayToken, Toast.LENGTH_SHORT).show();
                 loggedInCheck = responseArray.get(0).getId();
-                if(loggedInCheck.equals("wrong55")){
+
+                if (loggedInCheck.equals("wrong55")) {
                     Toast.makeText(getApplicationContext(), "Registeer", Toast.LENGTH_SHORT).show();
-                }else if(loggedInCheck.equals("wrong00")){
+                } else if (loggedInCheck.equals("wrong00")) {
                     Toast.makeText(getApplicationContext(), "Wrong password", Toast.LENGTH_SHORT).show();
-                }else{
-                    Toast.makeText(getApplicationContext(), "HTTP OK! :)", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    firstname = responseArray.get(0).getFirstname();
+                    lastname = responseArray.get(0).getLastname();
+                    natID = responseArray.get(0).getNatID();
+                    userPassword = responseArray.get(0).getPassword();
+                    regDate = responseArray.get(0).getRegDate();
+
+                    Intent toMain = new Intent(LoginActivity.this, MainActivity.class);
+
+                    toMain.putExtra("firstname", firstname);
+                    toMain.putExtra("lastname", lastname);
+                    toMain.putExtra("natID", natID);
+                    toMain.putExtra("userPassword", userPassword);
+                    toMain.putExtra("regDate", regDate);
+
+                    startActivity(toMain);
                 }
 
             }
