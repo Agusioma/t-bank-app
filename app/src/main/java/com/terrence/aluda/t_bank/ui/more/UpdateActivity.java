9de3c31892dc20b68c59927f5ce0c3527f7cf7a1
Agusioma <terryterrence200@gmail.com>
2021-11-25 15:21:55 +0300

    added update activity

---
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/more/UpdateActivity.java
@@ -0,0 +1,14 @@
+package com.terrence.aluda.t_bank.ui.more;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.terrence.aluda.t_bank.R;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
