188dd5f01bffe1a8bdd006d3fb11a33b24bdfaf6
Agusioma <terryterrence200@gmail.com>
2021-12-02 16:34:24 +0300

    MoreFRagment  updates

--- app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
+++ app/src/main/java/com/terrence/aluda/t_bank/ui/more/MoreFragment.java
@@ -1,9 +1,11 @@
 package com.terrence.aluda.t_bank.ui.more;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,13 +19,30 @@ import java.util.ArrayList;
 public class MoreFragment extends Fragment {
     private RecyclerView moreRV;
     private ArrayList<MoreModel> moreModelArrayList;
+    private TextView text_prof, text_prof1, text_prof2;
+    private SharedPreferences sharedPreferences;
+    private String loggedInCheck, firstname, lastname, email, natID, phonee, userPassword, regDate, phoneParam;
+
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_more, container, false);
         moreRV = root.findViewById(R.id.moreRV);
+        text_prof = root.findViewById(R.id.text_prof);
+        text_prof1 = root.findViewById(R.id.text_prof1);
+        text_prof2 = root.findViewById(R.id.text_prof2);
+
+        sharedPreferences = getActivity().getSharedPreferences("MyTax", 0);
+        firstname = sharedPreferences.getString("Name", "defaultValue");
+        lastname = sharedPreferences.getString("Last", "defaultValue");
+        phonee = sharedPreferences.getString("userPhone", "defaultValue");
+        email = sharedPreferences.getString("emailAddress", "defaultValue");
+
+        text_prof.setText(firstname+lastname);
+        text_prof1.setText(email);
+        text_prof2.setText(phonee);
 
         // here we have created new array list and added data to it.
         moreModelArrayList = new ArrayList<>();
-        moreModelArrayList.add(new MoreModel("Security","Change your sign-in details and password",R.drawable.ic_outline_security_24));
+        moreModelArrayList.add(new MoreModel("Security & Updates","Change your account details and password",R.drawable.ic_outline_security_24));
         moreModelArrayList.add(new MoreModel("Support","Email, call or find us on social media ",R.drawable.ic_outline_call_24));
         moreModelArrayList.add(new MoreModel("Sign out","",R.drawable.ic_outline_logout_24));
 
